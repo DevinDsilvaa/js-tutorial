f50754ef4443391ffece8bb12ec9e7a8a8cd238f
Devin D'silva <168718142+DevinDsilvaa@users.noreply.github.com>
2025-06-23 09:46:22 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";  // treat all js code as newer version.
+
+// alert(3 + 3)  --> we're using nodejs , not brower
+
+console.log(3
+    +
+    3) // code readability should be high which means standard and clear,understandable by humans
+
+console.log("Devin")
+
+
+let name = "Devin"
+let age = 20
+let isLoggedIn = true
+let state; // --> undefined
+let weather = null //--> Eg : to show exact error retrieving actual weather condition or temp
+
+// Data Types : Primitive
+// number => 2 to power 53
+// string => ""  or  ''
+// bigint
+// boolean
+// null => standalone value
+// undefined
+// symbol => uniqueness
+
+// Object
+
+console.log(typeof"Devin");
+console.log(typeof age);
+console.log(typeof undefined); // --> undefined
+console.log(typeof null); // --> object
+
+
+/* Refer code documentation from mdn or tc39.es */
