661afe36b87c90cce621d75961810c3c92380859
Devin D'silva <168718142+DevinDsilvaa@users.noreply.github.com>
2025-06-24 16:32:49 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "66xyz"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Conversion of datatype
+// "66" -> 66
+// "66xyz" -> NaN
+// true = 1 ; false = 0
+
+
+// let isLoggedIn = 0   //-->false
+// let isLoggedIn = 1   //-->true
+// let isLoggedIn = ""  //-->false
+let isLoggedIn = "Devin"    //-->true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let num = 66
+let stringNum = String(num)
+console.log(stringNum);
+console.log(typeof stringNum);
