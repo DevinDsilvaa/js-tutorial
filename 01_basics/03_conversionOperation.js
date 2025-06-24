dbfd19df65890214d91494b875527214ac5465b6
Devin D'silva <168718142+DevinDsilvaa@users.noreply.github.com>
2025-06-24 17:13:01 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
-let score = "66xyz"
+// let score = "66xyz"
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
@@ -16,13 +16,53 @@ let valueInNumber = Number(score)
 // let isLoggedIn = 0   //-->false
 // let isLoggedIn = 1   //-->true
 // let isLoggedIn = ""  //-->false
-let isLoggedIn = "Devin"    //-->true
+// let isLoggedIn = "Devin"    //-->true
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
 
-let num = 66
-let stringNum = String(num)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let num = 66
+// let stringNum = String(num)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+// *****************************OPERATIONS*******************************
+
+let value = 7
+let negVal = -value
+// console.log(negVal);
+
+// console.log(4+2);
+// console.log(4-2);
+// console.log(4*2);
+// console.log(4**2);
+// console.log(4/2);
+// console.log(4%2);
+
+let str1 = "Hello"
+let str2 = " Devin!"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+false);
+// console.log(+"");
+
+let n1,n2,n3
+n1 = n2 = n3 = 6 + 9
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
